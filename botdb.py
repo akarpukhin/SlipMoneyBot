f2e2588d9cf4e44d78bfe65fdcb58debc8915aef
chorus12 <pa151501@mail.ru>
2017-07-22 11:21:45 +0300

    Доработан класс event - добавлено поле created_by, вставлены комментарии

--- botdb.py
+++ botdb.py
@@ -24,51 +24,60 @@ class Event(Base):
 
 
 # таблица с описанием цели:
-# user_list_id - ссылка на список пользователей, участвующих в достижении цели
 # event_id - ссылка на событие, для которого создали цель - может быть пустой
 # goal_target - номинальная сумма, которую хотят собрать для достижения цели
 # goal_amount - текущая сумма, которую набрали
+# goal_type - тип сбора средств - фикс. итог - 1, фикс. сбор - 2, произвольный - 3
 # goal_date - дата, к которой хотят выполнить цель
+# goal_link_status - самостоятельная цель или связанная с событием
+# chat_id - идентификатор группы, в которой создана цель
+# is_active - живая ли цель
+# created_by - идентификатор телеграмм-пользователя создавшего цель
 
 # N.B. target и amount надо сделать decimal, но это будет позднее!!!
 
 class Goal(Base):
     __tablename__ = 'goal'
-# тип цели - самостоятельная -'u' или имеющая родительское мероприятие - 'l'
-# используется для таблицы goal - поле goal_type
-    GOAL_LINK = ['U', 'L']
 
     id = Column(Integer, primary_key=True)
-#    user_list_id = Column(Integer) убираем, т.к. отказались от списков пользователей
     event_id = Column(Integer)
     goal_target = Column(Integer)
     goal_amount = Column(Integer)
     goal_name = Column(String(50))
     goal_date = Column(DateTime)
-#   тип сбора средств - фикс. итог - 1, фикс. сбор - 2, произвольный - 3
     goal_type = Column(Integer)
-# самостоятельная цель или связанная с событием
     goal_link_status = Column(Boolean, default=False)
     chat_id = Column(Integer)
+    created_by = Column(Integer)
     is_active = Column(Boolean, default=True)
     users = relationship('User', secondary='list')
 
     def __init__(self, event_id=None, goal_target=0, goal_amount=0,
                  goal_name='empty', goal_date=datetime.today() + timedelta(days=10),
-                 goal_type=0, chat_id=None, is_active=True, goal_link_status=False):
+                 goal_type=0, goal_link_status=False, chat_id=None, created_by=None,
+                 is_active=True):
         self.goal_name = goal_name
         self.goal_date = goal_date
         self.goal_target = goal_target
         self.goal_type = goal_type
-        self.chat_id = chat_id
-        self.is_active = is_active
         self.goal_link_status = goal_link_status
+        self.chat_id = chat_id
+        self.created_by = created_by
+        self.is_active = is_active
 
     def __repr__(self):
-        return '<{}, {}, {}, {}, {}, {}, {}, {}, {}, {}>'.format(
-            self.id, self.event_id, self.goal_target, self.goal_amount,
-            self.goal_name, self.goal_date, self.goal_type, self.chat_id, self.is_active,
-            self.goal_link_status)
+        return '<{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}>'.format(
+            self.id,
+            self.event_id,
+            self.goal_target,
+            self.goal_amount,
+            self.goal_name,
+            self.goal_date,
+            self.goal_type,
+            self.goal_link_status,
+            self.chat_id,
+            self.created_by,
+            self.is_active)
 
 #    @validates('goal_type')
 #    def validate_goal_type(self, key, goal_type):
